04888add4dcfcf06154402f5bde786add86b63df
jaydunb12 <74875595+jaydunb12@users.noreply.github.com>
2023-03-15 14:19:08 -0400

    update

--- front/src/pages/home/Home.js
+++ front/src/pages/home/Home.js
@@ -8,6 +8,9 @@ const Home = () => {
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
     <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
         <Category />
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+          Thejayadad
+        </Typography>
     </Box>
     <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
     <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
